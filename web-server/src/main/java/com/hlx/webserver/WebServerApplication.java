e6a4f8be59ff49fd58e354f66e1789fdffba85c1
hlx <603773962@qq.com>
2018-08-13 17:37:39 +0800

    接口服务项目

---
+++ web-server/src/main/java/com/hlx/webserver/WebServerApplication.java
@@ -0,0 +1,12 @@
+package com.hlx.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerApplication.class, args);
+	}
+}
