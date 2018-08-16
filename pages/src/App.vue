26652c79bc43f12cae593b73060336b5ea390cfb
HYuT <470712340@qq.com>
2018-08-16 21:03:14 +0800

    :lipstick:demo

--- pages/src/App.vue
+++ pages/src/App.vue
@@ -12,11 +12,13 @@ export default {
 
 <style>
 #app {
+  position: fixed;
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
