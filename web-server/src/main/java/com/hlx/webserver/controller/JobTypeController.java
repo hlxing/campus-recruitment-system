174743c7ac8ccb1cfcb409aa0838f41c5a2c6910
hlx <603773962@qq.com>
2018-08-26 23:42:11 +0800

    :star:工作类型接口

---
+++ web-server/src/main/java/com/hlx/webserver/controller/JobTypeController.java
@@ -0,0 +1,69 @@
+package com.hlx.webserver.controller;
+
+import com.hlx.webserver.model.dto.req.JobTypeDTO;
+import com.hlx.webserver.model.po.ApiResult;
+import com.hlx.webserver.model.po.JobDirection;
+import com.hlx.webserver.model.po.JobType;
+import com.hlx.webserver.service.JobTypeService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @description: 工作类别控制层
+ * @author: hlx 2018-08-26
+ **/
+@RestController
+@RequestMapping("/jobType")
+public class JobTypeController {
+
+    private JobTypeService jobTypeService;
+
+    @Autowired
+    public JobTypeController(JobTypeService jobTypeService) {
+        this.jobTypeService = jobTypeService;
+    }
+
+    @ApiOperation("添加某工作方向下的工作类别")
+    @ApiImplicitParam(name = "jobDirectionId", value = "工作方向id")
+    @PostMapping("/add/{jobDirectionId}")
+    public ApiResult<String> add(@PathVariable("jobDirectionId") Integer jobDirectionId,
+                                 @RequestBody JobTypeDTO jobTypeDTO) {
+        ApiResult<String> apiResult = new ApiResult<>();
+        boolean addSuccess = jobTypeService.save(jobDirectionId, jobTypeDTO);
+        if (!addSuccess) {
+            apiResult.setStatus(404);
+        }
+        return apiResult;
+    }
+
+    @ApiOperation(value = "根据id删除工作类别")
+    @ApiImplicitParam(name = "id", value = "工作类别id")
+    @GetMapping("/delete/{id}")
+    public void deleteById(@PathVariable("id") Integer id) {
+        jobTypeService.deleteById(id);
+    }
+
+    @ApiOperation(value = "获取所有工作类别")
+    @GetMapping("/all")
+    public ApiResult<List<JobType>> getAll() {
+        ApiResult<List<JobType>> apiResult = new ApiResult<>();
+        List<JobType> jobTypes = jobTypeService.list();
+        apiResult.setData(jobTypes);
+        return apiResult;
+    }
+
+    @ApiOperation(value = "根据工作方向id查询类型")
+    @ApiImplicitParam(name = "jobDirectionId", value = "工作方向id")
+    @GetMapping("/{jobDirectionId}")
+    public ApiResult<List<JobType>> getByJobDirectionId(@PathVariable("jobDirectionId") Integer jobDirectionId) {
+        ApiResult<List<JobType>> apiResult = new ApiResult<>();
+        List<JobType> jobTypes = jobTypeService.listByJobDirectionId(jobDirectionId);
+        apiResult.setData(jobTypes);
+        return apiResult;
+    }
+
+}
---
+++ web-server/src/main/java/com/hlx/webserver/service/impl/JobTypeServiceImpl.java
@@ -0,0 +1,61 @@
+package com.hlx.webserver.service.impl;
+
+import com.hlx.webserver.dao.JobDirectionDao;
+import com.hlx.webserver.dao.JobTypeDao;
+import com.hlx.webserver.model.dto.req.JobTypeDTO;
+import com.hlx.webserver.model.po.JobDirection;
+import com.hlx.webserver.model.po.JobType;
+import com.hlx.webserver.service.JobTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @description: 工作类别服务实现层
+ * @author: hlx 2018-08-26
+ **/
+@Service
+public class JobTypeServiceImpl implements JobTypeService {
+
+    private JobTypeDao jobTypeDao;
+
+    private JobDirectionDao jobDirectionDao;
+
+    @Autowired
+    public JobTypeServiceImpl(JobTypeDao jobTypeDao, JobDirectionDao jobDirectionDao) {
+        this.jobTypeDao = jobTypeDao;
+        this.jobDirectionDao = jobDirectionDao;
+    }
+
+    @Override
+    public boolean save(Integer jobDirectionId, JobTypeDTO jobTypeDTO) {
+        // 检测工作方向和工作类别合法性
+        // 工作方向id存在,工作类别名称不重复
+        JobDirection jobDirection = jobDirectionDao.getById(jobDirectionId);
+        JobType testType = jobTypeDao.getByName(jobTypeDTO.getName());
+        if (jobDirection != null && testType == null) {
+            // 为增加类别后获取自增ID,进一步转换为JobType
+            JobType jobType = new JobType(jobTypeDTO.getName());
+            jobTypeDao.save(jobType);
+            jobTypeDao.saveDirectionType(jobDirectionId, jobType.getId());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void deleteById(Integer jobTypeId) {
+        jobTypeDao.deleteById(jobTypeId);
+    }
+
+    @Override
+    public List<JobType> list() {
+        return jobTypeDao.list();
+    }
+
+    @Override
+    public List<JobType> listByJobDirectionId(Integer jobDirectionId) {
+        return jobDirectionDao.getJobTypeByDirectionId(jobDirectionId);
+    }
+}
